48093d915c0ebf744ec0e1d1dcb9591f25eb56e1
Shuntian Liu <shuntian.liu@outlook.com>
2021-12-08 11:28:42 +0000

    moving fragmentation to separate module

--- apps/erlog/src/coordinator.erl
+++ apps/erlog/src/coordinator.erl
@@ -6,8 +6,20 @@
 
 -include_lib("kernel/include/logger.hrl").
 
--export([start_link/1, assign_task/1, finish_task/1, stop_coordinator/1]).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2]).
+-import(db_ops, [db_to_string/1]).
+
+-export([start_link/1, get_prog/1, get_static_db/1, get_num_tasks/1, assign_task/1,
+         finish_task/2, stop_coordinator/1]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+         code_change/3]).
+
+-record(coor_state,
+        {tasks :: [mr_task()],
+         num_tasks :: non_neg_integer(),
+         static_db :: dl_db_instance(),
+         non_static_prog :: dl_program()}).
+
+-type state() :: #coor_state{}.
 
 -ifdef(TEST).
 
@@ -27,11 +39,20 @@ start_link(ProgName) ->
 
 %% Synchronous call
 
+get_num_tasks(Pid) ->
+  gen_server:call(Pid, num_tasks).
+
+get_prog(Pid) ->
+  gen_server:call(Pid, prog).
+
+get_static_db(Pid) ->
+  gen_server:call(Pid, static_db).
+
 assign_task(Pid) ->
   gen_server:call(Pid, assign).
 
-finish_task(Pid) ->
-  gen_server:cast(Pid, finish).
+finish_task(Pid, Task) ->
+  gen_server:cast(Pid, {finish, Task}).
 
 stop_coordinator(Pid) ->
   gen_server:call(Pid, terminate).
@@ -40,151 +61,95 @@ stop_coordinator(Pid) ->
 
 % there is a couple of things we need to do here
 % read in rules and
+-spec init([string()]) -> {ok, state()}.
 init([ProgName]) ->
+  ok = file:del_dir_r("apps/erlog/test/tmp"),
+  ok = file:make_dir("apps/erlog/test/tmp"),
   {ok, Stream} = file:open("apps/erlog/test/eval_SUITE_data/" ++ ProgName, [read]),
   {Facts, Rules} = preproc:lex_and_parse(Stream),
   file:close(Stream),
   % preprocess rules
   Program = preproc:process_rules(Rules),
   ?LOG_DEBUG(#{input_prog => utils:to_string(Program)}),
-  ?LOG_DEBUG(#{input_data => Facts}),
+  ?LOG_DEBUG(#{input_data => db_to_string(Facts)}),
   % create EDB from input relations
   EDB = db_ops:from_list(Facts),
 
   % the coordinator would do a first round of evaluation to find all static relations
   NewDB = eval:imm_conseq(Program, EDB),
   FullDB = db_ops:add_db_unique(NewDB, EDB),
-
+  StaticDB = FullDB,
   % this is the program that will be sent to workers
   NonStaticProg = lists:filter(fun(R) -> not eval:static_relation(R, Program) end, Program),
 
-  hash_frag(FullDB, NonStaticProg, ?NUM_TASKS),
-  Tasks =
-    [#task{num = X,
-           state = idle,
-           type = evaluate}
-     || X <- lists:seq(1, ?NUM_TASKS)],
+  frag:hash_frag(FullDB, NonStaticProg, ?NUM_TASKS, 1, ?inter_dir),
+  Tasks = [tasks:new_task(X, 1) || X <- lists:seq(1, ?NUM_TASKS)],
+  ?LOG_DEBUG(#{tasks_after_coor_initialisation => Tasks}),
+  {ok,
+   #coor_state{tasks = Tasks,
+               num_tasks = ?NUM_TASKS,
+               static_db = StaticDB,
+               non_static_prog = NonStaticProg}}.
 
-  io:format("tasks ~p~n", [Tasks]),
-  {ok, [Tasks]}.
+handle_call(prog, _From, State = #coor_state{non_static_prog = NonStaticProg}) ->
+  ?LOG_DEBUG(#{assigned_prog_to_worker => db_to_string(State#coor_state.non_static_prog)}),
+  {reply, NonStaticProg, State};
+handle_call(static_db, _From, State = #coor_state{static_db = StaticDB}) ->
+  ?LOG_DEBUG(#{static_db_given_to_worker => StaticDB}),
+  {reply, StaticDB, State};
+handle_call(num_tasks, _From, State = #coor_state{num_tasks = NumTasks}) ->
+  {reply, NumTasks, State};
+handle_call(assign, _From, State = #coor_state{tasks = Tasks}) ->
+  {Task, NewTasks} = find_next_task(Tasks),
+  ?LOG_DEBUG(#{old_tasks => Tasks, new_tasks => NewTasks}),
+  {reply, Task, State#coor_state{tasks = NewTasks}};
+handle_call(terminate, _From, State) ->
+  {stop, normal, ok, State}.
 
-handle_call(assign, _From, Tasks) ->
+handle_cast({finish, Task}, State = #coor_state{tasks = Tasks}) ->
+  NewTasks = update_finished_task(Task, Tasks),
+  {noreply, State#coor_state{tasks = NewTasks}}.
 
-  {reply,
-   #task{num = 1,
-         state = idle,
-         type = wait},
-   Tasks};
-handle_call(terminate, _From, Tasks) ->
-  {stop, normal, ok, Tasks}.
-
-handle_cast({finish}, Tasks) ->
-  {noreply, Tasks}.
-
-handle_info(Msg, Tasks) ->
+handle_info(Msg, State) ->
   io:format("Unexpected message: ~p~n", [Msg]),
-  {noreply, Tasks}.
+  {noreply, State}.
 
-terminate(normal, _Tasks) ->
+terminate(normal, _State) ->
   io:format("coordinator terminated~n"),
   ok.
 
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
 %%% Private functions
 
-
-
-
-
--spec hash_and_write_one(dl_atom(), [integer()], integer(), file:io_device()) -> ok.
-hash_and_write_one(Atom, Cols, TaskNum, Stream) ->
-  ToHash = dl_repr:get_atom_args_by_index(Cols, Atom),
-  Num = erlang:phash2(ToHash, ?NUM_TASKS) + 1,
-  case Num == TaskNum of
-    true ->
-      io:format(Stream, "~s.~n", [utils:to_string(Atom)]);
-    false ->
-      ok
-  end.
-
 %%----------------------------------------------------------------------
 %% @doc
-%% Function: hash_and_write
-%% Purpose: given a bunch of atoms and columns of their args to hash on,
-%% this function would hash according to the given column and write
-%% them onto disk accordingly
-%% Args: Atoms is the bunch of atoms
-%% Returns:
-%% E.g. (
-%%  reachable(a, b)
-%%  reachable(b, c)
-%%  reachable(c, c)
-%%  reachable(c, d)
-%% )
-%% hashing on the second col, and this would give us something like
-%% file1 reachable(a, b)
-%% file2 reachable(b, c), reachable(c, c)
-%% file3 reachable(c, d)
-%%----------------------------------------------------------------------
--spec hash_and_write(dl_db_instance(), [integer()], integer(), file:io_device()) -> ok.
-hash_and_write(Atoms, Cols, TaskNum, Stream) ->
-  db_ops:foreach(fun(Atom) -> hash_and_write_one(Atom, Cols, TaskNum, Stream) end, Atoms).
-
-%% for each rule, check its body to see whether there will be a join,
-%% if so partition atoms with the same name as the rule's body in the db.
--spec part_by_rule(dl_db_instance(), dl_rule(), integer(), file:io_device()) ->
-                    dl_db_instance().
-part_by_rule(DB, Rule, TaskNum, Stream) ->
-  case Rule of
-    #dl_rule{body = [A1 = #dl_atom{}, A2 = #dl_atom{}]} ->
-      {C1, C2} = eval:get_overlap_cols(A1#dl_atom.args, A2#dl_atom.args),
-      {Atoms1, DBRest1} = db_ops:get_rel_by_pred_and_rest(A1#dl_atom.pred_sym, DB),
-      {Atoms2, DBRest2} = db_ops:get_rel_by_pred_and_rest(A2#dl_atom.pred_sym, DBRest1),
-      hash_and_write(Atoms1, C1, TaskNum, Stream),
-      hash_and_write(Atoms2, C2, TaskNum, Stream),
-      DBRest2;
-    #dl_rule{body = [#dl_atom{}]} ->
-      % TODO just put everything else into every file
-      % is this necessary?
-      DB
-  end.
-
-%%----------------------------------------------------------------------
-%% @doc
-%% Function: part_by_rules
-%% Purpose: Given all rules, look at each of them in turn, and partition
-%% the db instance according to each of them.
-%%
-%% @see part_by_rule/4
-
--spec part_by_rules(dl_db_instance(), [dl_rule()], integer(), file:io_device()) -> ok.
-part_by_rules(DB, [], _CurNum, _Stream) ->
-  case db_ops:is_empty(DB) of
-    true ->
-      ok;
-    false ->
-      io:format("non empty db, ~s~n", [db_ops:to_string(DB)])
-  end,
-  ok;
-part_by_rules(DB, [RH | RT], CurNum, Stream) ->
-  DBRest = part_by_rule(DB, RH, CurNum, Stream),
-  part_by_rules(DBRest, RT, CurNum, Stream).
-
-hash_frag_rec(_DB, _Rules, CurNum, TotNum) when CurNum > TotNum ->
-  ok;
-hash_frag_rec(DB, Rules, CurNum, TotNum) ->
-  FileName = io_lib:format("~stask-~w", [?INTER_DIR, CurNum]),
-  {ok, Stream} = file:open(FileName, [append]),
-  part_by_rules(DB, Rules, CurNum, Stream),
-  file:close(Stream),
-  hash_frag_rec(DB, Rules, CurNum + 1, TotNum).
-
-%%----------------------------------------------------------------------
-%% @doc
-%% Function: hash_frag
-%% Purpose: partition the db and write them onto disk
+%% Function: find_next_task
+%% Purpose: given a list of tasks, find the next one to be assigned.
+%%  if a task can be assigned that means
+%%  <ol>
+%%    <li>it is idle </li>
+%%    <li>@todo the worker has timedout </li>
+%%  </ol>
+%% if there is none available, then give a wait task
 %% Args:
-%% Returns: actual number of tasks being generated
+%% @returns the task that can be assigned, and the updated list of tasks.
+%% @end
 %%----------------------------------------------------------------------
--spec hash_frag(dl_db_instance(), dl_rule(), integer()) -> ok.
-hash_frag(DB, Rules, Num) ->
-  hash_frag_rec(DB, Rules, 1, Num).
+-spec find_next_task([mr_task()]) -> {mr_task(), [mr_task()]}.
+find_next_task(Tasks) ->
+  case lists:splitwith(fun(T) -> not tasks:is_idle(T) end, Tasks) of
+    {NonIdles, [IdleH | IdleT]} ->
+      NewTask = tasks:set_in_prog(IdleH),
+      {NewTask, NonIdles ++ [NewTask | IdleT]};
+    {_NonIdles, []} -> % there is no idle task in this case, return wait task
+      {tasks:new_wait_task(), Tasks}
+  end.
+
+-spec update_finished_task(mr_task(), [mr_task()]) -> [mr_task()].
+update_finished_task(Task, Tasks) ->
+  ?LOG_DEBUG(#{task_finished => Task}),
+  {L1, [L2H | L2T]} = lists:splitwith(fun(T) -> T =/= Task end, Tasks),
+  L2H2 = tasks:set_finished(L2H),
+  L1 ++ [L2H2 | L2T].
---
+++ apps/erlog/src/frag.erl
@@ -0,0 +1,127 @@
+-module(frag).
+
+-compile(export_all).
+
+-include("../include/data_repr.hrl").
+-include_lib("kernel/include/logger.hrl").
+
+-spec hash_and_write_one(dl_atom(),
+                         [integer()],
+                         integer(),
+                         integer(),
+                         file:io_device()) ->
+                          ok.
+hash_and_write_one(Atom, Cols, TaskNum, TotTasks, Stream) ->
+  ToHash = dl_repr:get_atom_args_by_index(Cols, Atom),
+  Num = erlang:phash2(ToHash, TotTasks) + 1,
+  case Num == TaskNum of
+    true ->
+      io:format(Stream, "~s.~n", [utils:to_string(Atom)]);
+    false ->
+      ok
+  end.
+
+%%----------------------------------------------------------------------
+%% @doc
+%% Function: hash_and_write
+%% Purpose: given a bunch of atoms and columns of their args to hash on,
+%% this function would hash according to the given column and write
+%% them onto disk accordingly
+%% Args: Atoms is the bunch of atoms
+%% Returns:
+%% E.g. (
+%%  reachable(a, b)
+%%  reachable(b, c)
+%%  reachable(c, c)
+%%  reachable(c, d)
+%% )
+%% hashing on the second col, and this would give us something like
+%% file1 reachable(a, b)
+%% file2 reachable(b, c), reachable(c, c)
+%% file3 reachable(c, d)
+%% @end
+%%----------------------------------------------------------------------
+-spec hash_and_write(dl_db_instance(),
+                     [integer()],
+                     integer(),
+                     integer(),
+                     file:io_device()) ->
+                      ok.
+hash_and_write(Atoms, Cols, TaskNum, TotTasks, Stream) ->
+  db_ops:foreach(fun(Atom) -> hash_and_write_one(Atom, Cols, TaskNum, TotTasks, Stream) end,
+                 Atoms).
+
+%% for each rule, check its body to see whether there will be a join,
+%% if so partition atoms with the same name as the rule's body in the db.
+-spec part_by_rule(dl_db_instance(), dl_rule(), integer(), integer(), file:io_device()) ->
+                    dl_db_instance().
+part_by_rule(DB, Rule, TaskNum, TotTasks, Stream) ->
+  case Rule of
+    #dl_rule{body = [A1 = #dl_atom{}, A2 = #dl_atom{}]} ->
+      {C1, C2} = eval:get_overlap_cols(A1#dl_atom.args, A2#dl_atom.args),
+      {Atoms1, DBRest1} = db_ops:get_rel_by_pred_and_rest(A1#dl_atom.pred_sym, DB),
+      {Atoms2, DBRest2} = db_ops:get_rel_by_pred_and_rest(A2#dl_atom.pred_sym, DBRest1),
+      hash_and_write(Atoms1, C1, TaskNum, TotTasks, Stream),
+      hash_and_write(Atoms2, C2, TaskNum, TotTasks, Stream),
+      DBRest2;
+    #dl_rule{body = [#dl_atom{}]} ->
+      % TODO just put everything else into every file
+      % is this necessary?
+      DB
+  end.
+
+%%----------------------------------------------------------------------
+%% @doc
+%% Function: part_by_rules
+%% Purpose: Given all rules, look at each of them in turn, and partition
+%% the db instance according to each of them.
+%%
+%% @see part_by_rule/4
+
+-spec part_by_rules(dl_db_instance(),
+                    [dl_rule()],
+                    integer(),
+                    integer(),
+                    file:io_device()) ->
+                     ok.
+part_by_rules(DB, [], _CurNum, _TotTasks, _Stream) ->
+  case db_ops:is_empty(DB) of
+    true ->
+      ok;
+    false ->
+      % TODO change this to LOG_NOTICE and add more handler
+      io:format("non empty db after examining all rules, ~s~n", [db_ops:to_string(DB)])
+  end,
+  ok;
+part_by_rules(DB, [RH | RT], CurNum, TotTasks, Stream) ->
+  DBRest = part_by_rule(DB, RH, CurNum, TotTasks, Stream),
+  part_by_rules(DBRest, RT, CurNum, TotTasks, Stream).
+
+-spec hash_frag_rec(dl_db_instance(),
+                    [dl_rule()],
+                    integer(),
+                    integer(),
+                    pos_integer(),
+                    file:filename()) ->
+                     ok.
+hash_frag_rec(_DB, _Rules, CurNum, TotNum, _StageNum, _DirPath) when CurNum > TotNum ->
+  ok;
+hash_frag_rec(DB, Rules, CurNum, TotNum, StageNum, DirPath) ->
+  % filename convention task-stage#-task#
+  FileName = io_lib:format("~stask-~w-~w", [DirPath, StageNum, CurNum]),
+  {ok, Stream} = file:open(FileName, [append]),
+  part_by_rules(DB, Rules, CurNum, TotNum, Stream),
+  file:close(Stream),
+  hash_frag_rec(DB, Rules, CurNum + 1, TotNum, StageNum, DirPath).
+
+%%----------------------------------------------------------------------
+%% @doc
+%% Function: hash_frag
+%% Purpose: partition the db into Num parts and write them onto disk
+%% Args:
+%% Returns: actual number of tasks being generated
+%% @end
+%%----------------------------------------------------------------------
+-spec hash_frag(dl_db_instance(), [dl_rule()], integer(), pos_integer(), file:filename()) -> ok.
+hash_frag(DB, Rules, TotTaskNum, StageNum, DirPath) ->
+  hash_frag_rec(DB, Rules, 1, TotTaskNum, StageNum, DirPath).
