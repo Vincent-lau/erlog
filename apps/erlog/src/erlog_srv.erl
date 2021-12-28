64368ef36e9db952a43df81cc6fa7dbf648742a7
Shuntian Liu <shuntian.liu@outlook.com>
2021-12-28 16:10:49 +0000

    chore: using append mode to logs and delete log when app starts

--- apps/erlog/src/erlog_srv.erl
+++ apps/erlog/src/erlog_srv.erl
@@ -9,12 +9,18 @@ init([]) ->
   %% Here we ignore what OTP asks of us and just do
   %% however we please.
   process_flag(trap_exit, true),
-  % ets:new(table, [named_table, public]),
+  ets:new(dl_atom_names, [named_table, public]),
+  case filelib:is_dir("log") of
+    true ->
+      file:delete("log/erlang.log");
+    false ->
+      ok
+    end,
   io:format("Hello, heavy world!~n"),
 
   {ok, []}.
 
-terminate(shutdown, State) ->
+terminate(shutdown, _State) ->
   io:format("terminate called~n"),
-  % ets:delete(table),
+  ets:delete(dl_atom_names),
   ok.
