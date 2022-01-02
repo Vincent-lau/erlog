7cd68ebe22a1f829a54b85f862528ed20e21cd56
Shuntian Liu <shuntian.liu@outlook.com>
2022-01-02 11:45:56 +0000

    feature: moving initialisation around

--- apps/erlog/test/distr_eval_SUITE.erl
+++ apps/erlog/test/distr_eval_SUITE.erl
@@ -9,7 +9,7 @@
 -import(dl_repr, [cons_atom/2]).
 
 all() ->
-  [{group, tc_many_workers}, tclarge4workers].
+  [tc6workers].
 
 groups() ->
   [{tc_many_workers, [], [tc4workers, tc3workers, tc6workers]}].
@@ -24,13 +24,13 @@ end_per_suite(_Config) ->
   application:stop(erlog).
 
 init_per_testcase(tclarge4workers, Config) ->
-  multi_worker_init(16, 4, "tc-large.dl", Config);
+  multi_worker_init(14, 4, "tc-large.dl", Config);
 init_per_testcase(tc3workers, Config) ->
   multi_worker_init(1, 3, "tc.dl", Config);
 init_per_testcase(tc4workers, Config) ->
   multi_worker_init(4, 4, "tc.dl", Config);
 init_per_testcase(tc6workers, Config) ->
-  multi_worker_init(9, 6, "tc.dl", Config).
+  multi_worker_init(8, 6, "tc.dl", Config).
 
 end_per_testcase(tclarge4workers, Config) ->
   multi_worker_stop(Config);
@@ -60,21 +60,21 @@ tc6workers(Config) ->
   dist_eval_tests(Config, "tc.dl", "reachable").
 
 multi_worker_init(InitialNum, NumWorkers, ProgName, Config) ->
-  {NodePids, NodeNames} = start_workers(InitialNum, NumWorkers),
-  TmpDir = get_tmp_dir("tc.dl", 3, Config),
+  TmpDir = get_tmp_dir("tc.dl", NumWorkers, Config),
   clean_tmp(TmpDir),
   {ok, Pid} = coordinator:start_link(?config(data_dir, Config) ++ ProgName, TmpDir),
+  {NodePids, NodeNames} = start_workers(InitialNum, NumWorkers),
   [{prog_name, ProgName},
    {worker_pids, NodePids},
    {worker_names, NodeNames},
-   {num_workers, 3},
+   {num_workers, NumWorkers},
    {tmp_dir, TmpDir},
    {coor_pid, Pid}
    | Config].
 
 multi_worker_stop(Config) ->
-  coordinator:stop_coordinator(),
-  stop_workers(Config).
+  stop_workers(Config),
+  coordinator:stop_coordinator().
 
 get_tmp_dir(ProgName, NumWorkers, Config) ->
   BaseName = filename:basename(ProgName, ".dl"),
@@ -104,33 +104,30 @@ start_workers(InitialNum, NumWorkers) ->
   NodeNames =
     [list_to_atom("worker" ++ integer_to_list(N) ++ "@vincembp")
      || N <- lists:seq(InitialNum, InitialNum + NumWorkers - 1)],
+  timer:sleep(1000),
+  R1 = erpc:multicall(NodeNames, worker, start, []),
+  ct:pal("rpc call to start workers result ~p~n", [R1]),
   {NodePids, NodeNames}.
 
 stop_workers(Config) ->
   NodePids = ?config(worker_pids, Config),
   NodeNames = ?config(worker_names, Config),
-  TmpL = rpc:multicall(NodeNames, worker, stop, []),
+  TmpL = erpc:multicall(NodeNames, worker, stop, []),
   ct:pal("results of calling worker stop ~p~n", [TmpL]),
   TmpM = lists:map(fun(Pid) -> erlang:port_close(Pid) end, NodePids),
   ct:pal("results of calling port close ~p~n", [TmpM]).
 
 dist_eval_tests(Config, ProgName, QryName) ->
   % open file and read program
-  timer:sleep(500),
   NodeNames = ?config(worker_names, Config),
-  {ResL, BadNodes} = rpc:multicall(NodeNames, worker, start, []),
-  rpc:multicall(NodeNames, worker, start_working, []),
-  ct:pal("rpc call  to start workers result ~p ~p~n", [ResL, BadNodes]),
+  R2 = erpc:multicall(NodeNames, worker, start_working, []),
+  ct:pal("rpc call to start_working ~p", [R2]),
 
   ok = wait_for_finish(),
   Res = dbs:read_db(?config(tmp_dir, Config) ++ "final_db"),
   ct:pal("Total result db is~n~s~n", [dbs:to_string(Res)]),
   ResQ = dbs:get_rel_by_pred(QryName, Res),
   Ans = dbs:read_db(?config(data_dir, Config) ++ QryName ++ ".csv", QryName),
-  ct:pal("The result database is:~n~s~n and the ans db is ~n~s~n",
-         [dbs:to_string(ResQ), dbs:to_string(Ans)]),
-  ct:pal("result - ans is ~n~s~n, ans - result is ~n~s~n",
-         [dbs:to_string(dbs:diff(ResQ, Ans)), dbs:to_string(dbs:diff(Ans, ResQ))]),
   true = dbs:equal(Ans, ResQ).
 
 wait_for_finish() ->
