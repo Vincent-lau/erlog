3ca91423b256dfeb31a320747ea75b8eb5bf2ca2
Shuntian Liu <shuntian.liu@outlook.com>
2022-01-09 11:27:07 +0000

    feat: adding supervisors for worker and coor

---
+++ apps/erlog/src/coor_sup.erl
@@ -0,0 +1,33 @@
+-module(coor_sup).
+
+-behaviour(supervisor).
+
+-export([start_link/1]).
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+start_link(ProgName) ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, [ProgName]).
+
+%% sup_flags() = #{strategy => strategy(),         % optional
+%%                 intensity => non_neg_integer(), % optional
+%%                 period => pos_integer()}        % optional
+%% child_spec() = #{id => child_id(),       % mandatory
+%%                  start => mfargs(),      % mandatory
+%%                  restart => restart(),   % optional
+%%                  shutdown => shutdown(), % optional
+%%                  type => worker(),       % optional
+%%                  modules => modules()}   % optional
+init([ProgName]) ->
+    SupFlags =
+        #{strategy => one_for_all,
+          intensity => 0,
+          period => 1},
+    ChildSpecs =
+        [#{id => main,
+           start => {coordinator, start_link, [ProgName]},
+           shutdown => 5000}],
+    {ok, {SupFlags, ChildSpecs}}.
+
+%% internal functions
