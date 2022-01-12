f9208a74c21281b680080d8dac0e495717eac022
Shuntian Liu <shuntian.liu@outlook.com>
2022-01-12 12:24:44 +0000

    test: adding tc2 distributed tests

--- apps/erlog/test/distr_eval_SUITE.erl
+++ apps/erlog/test/distr_eval_SUITE.erl
@@ -4,13 +4,13 @@
 
 -export([all/0, groups/0, init_per_group/2, end_per_group/2, init_per_testcase/2,
          end_per_testcase/2, init_per_suite/1, end_per_suite/1]).
--export([tc3workers/1, tc4workers/1, tc6workers/1, tclarge4workers/1, scc4workers/1,
-         rsg4workers/1, marrying4workers/1, nonlinear4workers/1]).
+-export([tc3workers/1, tc4workers/1, tc6workers/1, tclarge4workers/1, tc2_4workers/1,
+         scc4workers/1, rsg4workers/1, marrying4workers/1, nonlinear4workers/1]).
 
 -import(dl_repr, [cons_atom/2]).
 
 all() ->
-  [tc4workers, rsg4workers, nonlinear4workers, scc4workers].
+  [tc4workers, rsg4workers, nonlinear4workers, scc4workers, marrying4workers, tc2_4workers].
 
 groups() ->
   [{tc_many_workers, [], [tc4workers, tc3workers, tc6workers]}].
@@ -35,6 +35,8 @@ init_per_testcase(scc4workers, Config) ->
   multi_worker_init(4, "scc.dl", Config);
 init_per_testcase(tclarge4workers, Config) ->
   multi_worker_init(4, "tc-large.dl", Config);
+init_per_testcase(tc2_4workers, Config) ->
+  multi_worker_init(4, "tc2.dl", Config);
 init_per_testcase(tc3workers, Config) ->
   multi_worker_init(3, "tc.dl", Config);
 init_per_testcase(tc4workers, Config) ->
@@ -51,6 +53,8 @@ end_per_testcase(rsg4workers, Config) ->
 end_per_testcase(scc4workers, Config) ->
   multi_worker_stop(Config);
 end_per_testcase(tclarge4workers, Config) ->
+  multi_worker_stop(Config);
+end_per_testcase(tc2_4workers, Config) ->
   multi_worker_stop(Config);
 end_per_testcase(tc3workers, Config) ->
   multi_worker_stop(Config);
@@ -75,10 +79,13 @@ rsg4workers(Config) ->
   dist_eval_tests(Config, "rsg").
 
 tclarge4workers(Config) ->
-  dist_eval_tests(Config,  "tc_large").
+  dist_eval_tests(Config, "tc_large").
+
+tc2_4workers(Config) ->
+  dist_eval_tests(Config, "reachable").
 
 tc3workers(Config) ->
-  dist_eval_tests(Config,  "reachable").
+  dist_eval_tests(Config, "reachable").
 
 tc4workers(Config) ->
   dist_eval_tests(Config, "reachable").
@@ -87,7 +94,7 @@ tc6workers(Config) ->
   dist_eval_tests(Config, "reachable").
 
 scc4workers(Config) ->
-  dist_eval_tests(Config,  "scc").
+  dist_eval_tests(Config, "scc").
 
 multi_worker_init(NumWorkers, ProgName, Config) ->
   TmpDir = get_tmp_dir(ProgName, NumWorkers, Config),
@@ -95,11 +102,7 @@ multi_worker_init(NumWorkers, ProgName, Config) ->
   ct:pal("program name ~p~n", [ProgName]),
   {ok, Pid} = coordinator:start_link(?config(program_dir, Config) ++ ProgName, TmpDir),
   Cfg = start_workers(NumWorkers),
-  [{prog_name, ProgName},
-   {worker_cfg, Cfg},
-   {tmp_dir, TmpDir},
-   {coor_pid, Pid}
-   | Config].
+  [{prog_name, ProgName}, {worker_cfg, Cfg}, {tmp_dir, TmpDir}, {coor_pid, Pid} | Config].
 
 multi_worker_stop(Config) ->
   stop_workers(Config),
@@ -127,17 +130,15 @@ start_workers(NumWorkers) ->
   ct:pal("results of all_start ~p~n", [R]),
   Cfg.
 
-
 stop_workers(Config) ->
   R = dconfig:stop_cluster(?config(worker_cfg, Config)),
   ct:pal("results of stopping workers ~p~n", [R]).
-
 
 dist_eval_tests(Config, QryName) ->
   WorkerCfg = ?config(worker_cfg, Config),
   dconfig:all_work(WorkerCfg),
 
-  ok = wait_for_finish(),
+  ok = coordinator:wait_for_finish(5000, 500),
   Res = dbs:read_db(?config(tmp_dir, Config) ++ "final_db"),
   ct:pal("Total result db is~n~s~n", [dbs:to_string(Res)]),
   ResQ = dbs:get_rel_by_pred(QryName, Res),
@@ -145,24 +146,3 @@ dist_eval_tests(Config, QryName) ->
   Ans = dbs:read_db(?config(program_dir, Config) ++ QryName ++ ".csv", QryName),
   ct:pal("Ans db is ~n~s~n", [dbs:to_string(Ans)]),
   true = dbs:equal(Ans, ResQ).
-
-wait_for_finish() ->
-  register(tester, self()),
-  spawn(fun Waiter() ->
-              case coordinator:done() of
-                true ->
-                  ct:pal("The coordinator has finished its job.~n"),
-                  tester ! done;
-                false ->
-                  timer:sleep(500),
-                  ct:pal("still waiting, currently the coordinator is in stage ~p of execution~n",
-                         [coordinator:get_current_stage_num()]),
-                  Waiter()
-              end
-        end),
-  receive
-    done ->
-      ok
-  after 10000 ->
-    timeout
-  end.
