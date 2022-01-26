e4b91eac48d1a4ed0b3d3fc03c5a81db0edcf871
Shuntian Liu <shuntian.liu@outlook.com>
2022-01-26 13:41:07 +0000

    chore: adding citation data

--- data/subset.py
+++ data/subset.py
@@ -9,5 +9,5 @@ with open('hep-th-citations', 'r') as in_file:
         break
       l = line.split()
 
-      out_file.write(f"link(\"a{int(l[0])}\", \"a{int(l[1])}\")\n") 
+      out_file.write(f"link(\"a{int(l[0])}\", \"a{int(l[1])}\").\n") 
       i += 1
