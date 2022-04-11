40ba8790315489911cfb851d75e54b0ad52f7f5b
Shuntian Liu <shuntian.liu@outlook.com>
2022-04-11 11:38:07 +0100

    tests:more tests for negation

--- apps/erlog/test/eval_SUITE.erl
+++ apps/erlog/test/eval_SUITE.erl
@@ -2,23 +2,28 @@
 
 -include_lib("common_test/include/ct.hrl").
 
--export([all/0, init_per_suite/1, end_per_suite/1]).
+-export([all/0, init_per_suite/1, end_per_suite/1, groups/0]).
 -export([tc_tests/1, tc2_tests/1, tc_large_tests/1, rsg_tests/1,
-         marrying_a_widower_tests/1, nonlinear_ancestor_tests/1, scc_tests/1, 
-         pointsto_tests/1, indirect_tests/1]).
+         marrying_a_widower_tests/1, nonlinear_ancestor_tests/1, scc_tests/1, pointsto_tests/1,
+         indirect_tests/1, unreachable_tests/1]).
 
 -import(dl_repr, [cons_atom/2, cons_const/1]).
 
 all() ->
-  [tc_tests,
-   tc2_tests,
-   rsg_tests,
-   marrying_a_widower_tests,
-   scc_tests,
-   nonlinear_ancestor_tests,
-   pointsto_tests,
-   indirect_tests
-  ].
+  [{group, positive_datalog}, {group, negative_datalog}].
+
+groups() ->
+  [{positive_datalog,
+    [shuffle],
+    [tc_tests,
+     tc2_tests,
+     rsg_tests,
+     tc_large_tests,
+     marrying_a_widower_tests,
+     scc_tests,
+     nonlinear_ancestor_tests,
+     pointsto_tests]},
+   {negative_datalog, [shuffle], [indirect_tests, unreachable_tests]}].
 
 ets_owner() ->
   receive
@@ -38,7 +43,7 @@ init_per_suite(Config) ->
 end_per_suite(Config) ->
   ?config(table_owner, Config) ! stop.
 
-eval_tests(Config, ProgName)->
+eval_tests(Config, ProgName) ->
   % open file and read program
   {Facts, Rules} = preproc:lex_and_parse(file, ?config(program_dir, Config) ++ ProgName),
   QryNames = preproc:get_output_name(?config(program_dir, Config) ++ ProgName),
@@ -61,7 +66,10 @@ eval_tests(Config, ProgName)->
          [lists:map(fun dbs:to_string/1, ResQL), lists:map(fun dbs:to_string/1, AnsL)]),
 
   ct:pal("ResQ - Ans ~n~s~n, Ans-ResQ ~n~s~n",
-         [dbs:to_string(dbs:subtract(hd(ResQL), hd(AnsL))), dbs:to_string(dbs:subtract(hd(AnsL), hd(ResQL)))]),
+         [dbs:to_string(
+            dbs:subtract(hd(ResQL), hd(AnsL))),
+          dbs:to_string(
+            dbs:subtract(hd(AnsL), hd(ResQL)))]),
 
   true = lists:all(fun({ResQ, Ans}) -> dbs:equal(ResQ, Ans) end, lists:zip(ResQL, AnsL)).
 
@@ -72,7 +80,7 @@ tc2_tests(Config) ->
   eval_tests(Config, "tc2.dl").
 
 tc_large_tests(Config) ->
-  eval_tests(Config, "tc-large.dl" ).
+  eval_tests(Config, "tc-large.dl").
 
 nonlinear_ancestor_tests(Config) ->
   eval_tests(Config, "non-linear-ancestor.dl").
@@ -91,3 +99,6 @@ pointsto_tests(Config) ->
 
 indirect_tests(Config) ->
   eval_tests(Config, "indirect.dl").
+
+unreachable_tests(Config) ->
+  eval_tests(Config, "unreachable.dl").
--- apps/erlog/test/neg_tests.erl
+++ apps/erlog/test/neg_tests.erl
@@ -2,8 +2,6 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--include("../include/data_repr.hrl").
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% TESTS DESCRIPTIONS %%%
@@ -11,6 +9,14 @@
 
 neg_pred_test_() ->
   [{"Basic negation test", {setup, fun start/0, fun neg_link/1}}].
+
+is_stratifiable_test_() ->
+  [{"cycle must contain negation", {setup, fun start2/0, fun cycle_no_negation/1}},
+  {"one negative edge in the cycle means no stratifiable", 
+    {setup, fun start3/0, fun one_neg_edge/1}}].
+
+compute_stratification_test_() ->
+  [{"compute stratification for unreachable", {setup, fun start4/0, fun unreachable_prog/1}}].
 
 %%%%%%%%%%%%%%%%%%%%%%%
 %%% SETUP FUNCTIONS %%%
@@ -36,6 +42,38 @@ start() ->
   Atom = dl_repr:cons_atom("link", ["a", "b"]),
   {Atom, Facts}.
 
+start2() ->
+  Input = 
+  "
+    reachable(X, Y) :- link(X, Y).
+    reachable(X, Y) :- link(X, Z), reachable(Z, Y).
+    indirect(X, Y) :- reachable(X, Y), !link(X, Y).
+  ",
+  {_F, Program} = preproc:lex_and_parse(str, Input),
+  Program.
+
+start3() ->
+  Input =
+  "
+    p(X) :- !q(X).
+    q(X) :- p(X).
+  ",
+  {_F, Program} = preproc:lex_and_parse(str, Input),
+  Program.
+
+
+start4() ->
+  Input =
+  "
+    reachable(X,Y) :- link(X,Y).
+    reachable(X,Y) :- link(X,Z), reachable(Z,Y).
+    node(X) :- link(X,Y).
+    node(Y) :- link(X,Y).
+    unreachable(X,Y) :- node(X), node(Y), !reachable(X,Y).
+  ",
+  {_F, Program} = preproc:lex_and_parse(str, Input),
+  Program.
+
 %%%%%%%%%%%%%%%%%%%%
 %%% ACTUAL TESTS %%%
 %%%%%%%%%%%%%%%%%%%%
@@ -57,3 +95,27 @@ neg_link({Atom, EDB}) ->
     dl_repr:cons_atom("link", ["d", "d"])
   ]),
   ?_assert(dbs:equal(Negated, Ans)).
+
+cycle_no_negation(Program) ->
+  ?_assert(neg:is_stratifiable(Program)).
+
+one_neg_edge(Program) ->
+  ?_assert(not neg:is_stratifiable(Program)).
+
+unreachable_prog(Program) ->
+  Strata = neg:compute_stratification(Program),
+  PS1 = 
+  "
+    reachable(X,Y) :- link(X,Y).
+    reachable(X,Y) :- link(X,Z), reachable(Z,Y).
+  ",
+  PS2 = "
+    node(X) :- link(X,Y).
+    node(Y) :- link(X,Y).
+  ",
+  {_F1, P1} = preproc:lex_and_parse(str, PS1),
+  {_F2, P2} = preproc:lex_and_parse(str, PS2),
+  {_F3, P3} = preproc:lex_and_parse(str, "unreachable(X,Y) :- node(X), node(Y), !reachable(X,Y)."),
+  ?_assertEqual([P1, P2, P3], Strata).
+  
+  
