9c54bc9839d73b4c2d96195afdbc2527ce96e1f5
Shuntian Liu <shuntian.liu@outlook.com>
2022-04-23 10:50:26 +0100

    feat:starting mode

--- apps/erlog/src/erlog_srv.erl
+++ apps/erlog/src/erlog_srv.erl
@@ -1,26 +1,39 @@
 -module(erlog_srv).
 
--export([start_link/0, init/1]).
+-export([start_link/0, init/1, start_link/1]).
+
+start_link(ProgName) ->
+  gen_server:start_link(?MODULE, [ProgName], []).
 
 start_link() ->
-  gen_server:start_link(?MODULE, [], []).
+  gen_server:start(?MODULE, [], []).
 
-init([]) ->
+init([ProgName]) ->
   %% Here we ignore what OTP asks of us and just do
   %% however we please.
   process_flag(trap_exit, true),
   ets:new(dl_atom_names, [named_table, public]),
-  case filelib:is_dir("log") of
-    true ->
-      file:delete("log/erlang.log");
-    false ->
-      ok
-  end,
   lager:debug("Application Erlog started!"),
-
+  Name = hd(string:tokens(atom_to_list(node()), "@")),
+  case string:slice(Name, 0, 4) of
+    "work" ->
+      lager:info("started in worker mode"),
+      worker_sup:start_link();
+    "coor" ->
+      lager:info("started in coordinator mode"),
+      coor_sup:start_link(ProgName);
+    "nono" ->
+      lager:info("no node name given, do nothing", [node()]);
+    _Other ->
+      lager:info("some strange node name ~p", [node()])
+  end,
+  {ok, []};
+init([]) ->
+  ets:new(dl_atom_names, [named_table, public]),
+  lager:debug("Application Erlog started!"),
+  lager:info("no program name given, do nothing"),
   {ok, []}.
 
 terminate(shutdown, _State) ->
-  io:format("terminate called~n"),
   ets:delete(dl_atom_names),
   ok.
--- apps/erlog/src/erlog_sup.erl
+++ apps/erlog/src/erlog_sup.erl
@@ -7,13 +7,13 @@
 
 -behaviour(supervisor).
 
--export([start_link/0]).
+-export([start_link/1, start_link/0]).
 -export([init/1]).
 
 -define(SERVER, ?MODULE).
 
-start_link({Mode, ProgName}) ->
-    supervisor:start_link({local, ?SERVER}, ?MODULE, [Mode, ProgName]).
+start_link(ProgName) ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, [ProgName]).
 
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
@@ -27,6 +27,16 @@ start_link() ->
 %%                  shutdown => shutdown(), % optional
 %%                  type => worker(),       % optional
 %%                  modules => modules()}   % optional
+init([ProgName]) ->
+    SupFlags =
+        #{strategy => one_for_all,
+          intensity => 0,
+          period => 1},
+    ChildSpecs =
+        [#{id => main,
+           start => {erlog_srv, start_link, [ProgName]},
+           shutdown => 5000}],
+    {ok, {SupFlags, ChildSpecs}};
 init([]) ->
     SupFlags =
         #{strategy => one_for_all,
