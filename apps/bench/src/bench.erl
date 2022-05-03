dc7fdfd1cc3ecb35987515297b12b35585bf492b
Shuntian Liu <shuntian.liu@outlook.com>
2022-05-03 13:49:11 +0100

    chore:bench and profiling minor change

--- apps/bench/src/bench.erl
+++ apps/bench/src/bench.erl
@@ -5,19 +5,25 @@
 -define(res_path, "apps/bench/results/timing_res.txt").
 
 main(_Args) ->
+  no_fault().
+
+straggle() ->
   {ok, Stream} = file:open(?res_path, [append]),
   io:format(Stream, "----------new experiment-----------~n", []),
   io:format(Stream, "~p~n", [calendar:local_time()]),
   lists:foreach(fun(P) ->
+                   Cmd = io_lib:format("./apps/bench/bench_program/gen_and_run.sh ~p", [150]),
+                   io:format("results of generating graph ~s~n", [os:cmd(Cmd)]),
                    application:start(erlog),
+                   application:ensure_all_started([erlog]),
                    timing:start({straggle, P * 0.1}),
                    application:stop(erlog)
                 end,
-                lists:seq(2, 5)),
+                [5]),
   io:format(Stream, "---------end of experiment---------~n", []),
   file:close(Stream).
 
-no_fault(_Args) ->
+no_fault() ->
   {ok, Stream} = file:open(?res_path, [append]),
   io:format(Stream, "~n~n----------new experiment-----------~n", []),
   io:format(Stream, "experiment start time ~p~n", [calendar:local_time()]),
@@ -28,8 +34,7 @@ no_fault(_Args) ->
                    timing:start(success),
                    application:stop(erlog)
                 end,
-                [50, 100, 150, 200, 250]),
-  % lists:foreach(fun (P) -> timing:start({failure, 0.1*P}) end, lists:seq(0, 9)),
+                [10]),
   io:format(Stream, "experiment end time ~p~n", [calendar:local_time()]),
   io:format(Stream, "---------end of experiment---------~n", []),
   file:close(Stream).
--- apps/bench/src/profiling.erl
+++ apps/bench/src/profiling.erl
@@ -1,25 +1,45 @@
 -module(profiling).
 
 -compile(export_all).
+-compile(nowarn_export_all).
 
 -define(PROG, "apps/erlog/bench_program/tc_bench.dl").
--define(BENCH_DIR, "apps/erlog/bench/").
+-define(BENCH_DIR, "apps/bench/results/").
 
 prof_single(fprof) ->
   fprof:trace(start, ?BENCH_DIR ++ "fprof.trace"),
-  erlog_worker:run_program(single, ?PROG, "reachable"),
+  timing:start(),
   fprof:trace(stop),
   fprof:profile(file, ?BENCH_DIR ++ "fprof.trace"),
   fprof:analyse({dest, ?BENCH_DIR ++ "fprof.analysis"});
 prof_single(flame) ->
   eflame:apply(normal_with_children,
                ?BENCH_DIR ++ "stacks.out",
-               erlog_worker,
-               run_program,
-               [single, ?PROG, "reachable"]),
+               timing,
+               start,
+               []),
   os:cmd("./_build/default/lib/eflame/stack_to_flame.sh < "
          ++ ?BENCH_DIR
          ++ "stacks.out"
          ++ " > "
          ++ ?BENCH_DIR
-         ++ "flame.svg").
+         ++ "flame.svg");
+prof_single(worker) ->
+  eflame:apply(normal_with_children,
+               ?BENCH_DIR ++ "worker.out",
+               worker,
+               start_working_sync,
+               []),
+  os:cmd("./_build/default/lib/eflame/stack_to_flame.sh < "
+         ++ ?BENCH_DIR
+         ++ "worker.out"
+         ++ " > "
+         ++ ?BENCH_DIR
+         ++ "worker_flame.svg");
+prof_single(fprof_worker) ->
+  fprof:trace(start, ?BENCH_DIR ++ "worker_opt.trace"),
+  worker:start_working_sync(),
+  fprof:trace(stop),
+  fprof:profile(file, ?BENCH_DIR ++ "worker_opt.trace"),
+  fprof:analyse({dest, ?BENCH_DIR ++ "worker_opt.analysis"}),
+  os:cmd("erlgrind apps/bench/results/worker_opt.analysis apps/bench/results/worker_opt.cgrind").
